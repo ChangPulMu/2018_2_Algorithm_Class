15f5caca1ae348765c8d14908b64754ca249c2bb
ChangPulMu <16906@naver.com>
2019-01-24 09:57:55 +0900

    Set of Subsets Problem Algorithm, C++로 알고리즘 교재 예제랑 임의의 자작 예제 구현

---
+++ textbook_set_of_subsets_problem.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+#define N 4
+
+int w[N] = { 3,4,5,6 }, W = 13;
+std::string include[N+1];
+
+void sum_of_subsets(int, int, int);
+bool promising(int, int, int);
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 0; i < N; i++)
+		sum += w[i];
+
+	sum_of_subsets(0, 0, sum);
+
+	return 0;
+}
+
+void sum_of_subsets(int i, int weight, int total)
+{
+	if (promising(i, weight, total))
+	{
+		if (weight == W)
+		{
+			for (int j = 1; j <= i; j++)
+				std::cout << include[j] << std::endl;
+			std::cout << std::endl;
+		}
+		else
+		{
+			include[i + 1] = "yes";
+			sum_of_subsets(i + 1, weight + w[i], total - w[i]);
+			include[i + 1] = "no";
+			sum_of_subsets(i + 1, weight, total - w[i]);
+		}
+	}
+}
+
+bool promising(int i, int weight, int total)
+{
+	return ((((weight + total) >= W)) && ((weight == W) || ((weight + w[i]) <= W)));
+}
